313ed54d84a11a043dcda5ef02bbc4abb6a3a200
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-06 01:12:29 +0000

    lab 2 update part 2

--- labs/2-associations.rb
+++ labs/2-associations.rb
@@ -4,13 +4,18 @@
 # rails runner labs/2-associations.rb
 
 # 1. write code to display each contact (you can reuse the previous lab's code) and include the contact's company name, e.g.:
-
 # ---------------------------------
 # Contacts: 4
 # Andy Jassy - andy@amazon.com - Amazon.com, Inc.
 # Craig Federighi - craig@apple.com - Apple Inc.
 # Elon Musk - elon@tesla.com - Tesla, Inc.
 # Tim Cook - tim@apple.com - Apple Inc.
+
+contacts = Contact.all
+puts "Contacts: #{Contact.all.count}"
+for contact in contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+end
 
 # 2. similar to above, but this time organized by company, write code to display each company (name) and its contacts, e.g.:
 
@@ -24,3 +29,13 @@
 #
 # Tesla, Inc.
 # Elon Musk - elon@tesla.com
+
+companies = Company.all
+for company in companies
+    puts company.name
+    contacts = company.contacts
+    for contact in contacts
+        puts  "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+    end
+    puts "" #this puts "" is just a line break
+end
