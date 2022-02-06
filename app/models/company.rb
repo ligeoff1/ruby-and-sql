f45dda15d48b41343c8cd025f89d209c9a9e27d0
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-06 01:00:54 +0000

    code-along 2 update

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ApplicationRecord
+    has_many :contacts 
+    
+    #def contacts
+    #    Contact.where({company_id: id})
+    #end
+
 end
--- code-along/2-associations.rb
+++ code-along/2-associations.rb
@@ -4,7 +4,19 @@
 # rails runner code-along/2-associations.rb
 
 # 1. Query all of the contacts at Apple Inc.
+apple = Company.where({name: "Apple"})[0]
+
+#contacts = Contact.where({company_id: apple.id}) 
+#We know that we are going to want to use ^^ this alot, so it is better to create an association between company/contacts within 
+#the model. We are just copying this into the model so we can use .contacts 
+
+contacts = apple.contacts
+for contact in apple.contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+end
 
 # 2. add association to contacts in Company model
+
+
 
 # 3. add association to company in Contact model
