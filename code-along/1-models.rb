ab2474c25306bf97b0d18630fa582045904bd7eb
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-06 00:14:07 +0000

    Code along updates

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -5,7 +5,7 @@
 # cat log/development.log - this shows you all of the items in a file
 
 # **************************
-# Don't change or move
+# Don't change or move - this deletes all rows in the company every time we run this file so we do not duplicate every time 
 Company.destroy_all
 # **************************
 
@@ -14,50 +14,57 @@ Company.destroy_all
 puts "There are #{Company.all.count} companies" #Company.all.count = "SELECT COUNT (*) FROM companies" - basically what you are doing 
 
 # 2. create new companies
+# Create Company #1
 values = {
     name:"Apple",
     url:"https://apple.com", 
     city: "Cupertino", 
     state: "CA"
 }
-
-company = Company.new(values)
-company.save #insert into a row on the table (enter into database)/hold this in memory 
+company = Company.new(values) #just holding in memory and don't exist anywhere else, once done running if you dont save it'll disappear
+company.save #actually inserting into a row on the table (enter into database) - connection back to SQL 
 puts "There are #{Company.all.count} companies"
 
+#Create Company #2
 values = {
     name:"Amazon",
     url:"https://amazon.com", 
     city: "Seattle", 
     state: "WA"
 }
-
 company = Company.new(values)
 company.save #insert into the database
 puts "There are #{Company.all.count} companies"
 
-
+#Create Company #3 - using a different way to assign values to a column 
 company = Company.new
-company.write_attribute(:name, "Tesla") # or company.name = "Tesla"
-company.write_attribute(:url, "https://tesla.com") # or company.url = "https://tesla.com"
+company.write_attribute(:name, "Tesla") 
+company.write_attribute(:url, "https://tesla.com") 
 company.write_attribute(:city, "Palo Alto") 
 company.write_attribute(:state, "CA")
 company.save
 puts "There are #{Company.all.count} companies"
 
-# 3. query companies table
-#puts Company.all.inspect #creates arrays, and basically a hash 
+#OR another way to create 
+#company.name = "Tesla"
+#company.url = "https://tesla.com"
+#company.city = "Palo Alto"
+#company.state = "CA"
+#company.save
 
+# 3. query companies table
+#puts Company.all.inspect the data is wrapped in square brackets (array) and each element in the arrays are basically a has  
 #companies = Company.where({state:"CA"}) #adding a WHERE clause - state is the value we are searching by and the paranthesis is what viewing
 #puts companies.inspect
 
+puts Company.where({state:"CA", name:"Apple"}).count
 apple = Company.where({state:"CA", name: "Apple"})[0] #adding a WHERE clause CA state companies with name apple, the brackers create the index. So saying show the very first instance where this is correct
 puts apple.inspect 
 
 # 4. read column values from row
-puts apple.read_attribute(:url) #retruns value for that specifict column/attribute 
-puts "#{apple.name} has a website: #{apple.url}"
-
+puts apple.read_attribute(:url) #reruns value for that specific column/attribute 
+puts "#{apple.read_attribute(:name)} has a website #{apple.read_attribute(:url)}"
+puts "#{apple.name} has a website #{apple.url}"
 
 # 5. update attribute value - adding a slogan
 apple.write_attribute(:slogan, "Think Different.")
